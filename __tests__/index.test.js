cb52c4cf5d84ef0b6de524498cfab8f7ffeca4e8
EldarOpera <eldar.akhmedov.17@gmail.com>
2023-03-16 23:11:21 +0300

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,12 +7,12 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('flat JSON test', () => {
-    const filepath1 = getPath('flat_file1.json');
-    const filepath2 = getPath('flat_file2.json');
-    const expectedFilepath = getPath('expected_json.txt');
-    
-    expect(genDiff(filepath1, filepath2)).toEqual(readFileSync(expectedFilepath));
+  const filepath1 = getFixturePath('flat_file1.json');
+  const filepath2 = getFIxturePath('flat_file2.json');
+  const expectedFilePath = getFixturePath('expected_json.txt');
+
+  expect(genDiff(filepath1, filepath2)).toEqual(readFileSync(expectedFilePath), 'utf-8');
 });
