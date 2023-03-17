b71f35b0ea1bfb47593a785291c6c1f29d88b52a
EldarOpera <eldar.akhmedov.17@gmail.com>
2023-03-17 11:09:58 +0300

    Update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,11 +8,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-test('flat JSON test', () => {
+test('flat .json test', () => {
   const filepath1 = getFixturePath('flat_file1.json');
-  const filepath2 = getFIxturePath('flat_file2.json');
-  const expectedFilePath = getFixturePath('expected_json.txt');
+  const filepath2 = getFixturePath('flat_file2.json');
+  const expected = readFile('expected_json.txt');
 
-  expect(genDiff(filepath1, filepath2)).toEqual(readFileSync(expectedFilePath), 'utf-8');
+  expect(genDiff(filepath1, filepath2)).toEqual(expected);
 });
