a4659e0919e5371865ced3f95e622a9ca96e30ee
EldarOpera <eldar.akhmedov.17@gmail.com>
2023-03-18 12:49:52 +0300

    Improve bugs

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,20 +3,20 @@ import { fileURLToPath } from 'url';
 import { getPath, getDir, readFile } from '../src/helpers.js';
 import genDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url); // /home/eldie/frp46/__tests__/index.test.js
-const __dirname = getDir(__filename); // /home/eldie/frp46/__tests__
+const __filename = fileURLToPath(import.meta.url); // /home/runner/work/frontend-project-46/__tests__/index.test.js
+const __dirname = getDir(__filename); // /home/runner/work/frontend-project-46/__tests__
 
 test('flat .json test', () => {
   const filepath1 = getPath(__dirname, '../__fixtures__', 'flat1.json');
-  const filepath2 = getPath(__dirname, '../__fixtures__', 'flat22.json');
+  const filepath2 = getPath(__dirname, '../__fixtures__', 'flat2.json');
   const expectedJSON = readFile(getPath(__dirname, '../__fixtures__', 'flat_expected.txt'));
 
   expect(genDiff(filepath1, filepath2)).toEqual(expectedJSON);
 });
 
 test('flat .yml test', () => {
-  const filepath1 = getPath(__dirname, '../__fixtures__', 'flat_file1.yml');
-  const filepath2 = getPath(__dirname, '../__fixtures__', 'flat_file2.yml');
+  const filepath1 = getPath(__dirname, '../__fixtures__', 'flat1.yml');
+  const filepath2 = getPath(__dirname, '../__fixtures__', 'flat2.yml');
   const expectedYML = readFile(getPath(__dirname, '../__fixtures__', 'flat_expected.txt'));
 
   expect(genDiff(filepath1, filepath2)).toEqual(expectedYML);
