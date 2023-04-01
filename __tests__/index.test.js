f9ea254cdcbac800fa0aeb62f8712fab6736a928
EldarOpera <eldar.akhmedov.17@gmail.com>
2023-04-01 12:47:35 +0300

    Fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { getDir, readFile } from '../src/helpers.js';
@@ -7,24 +7,34 @@ import genDiff from '../index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = getDir(__filename);
 
-const stylishExpected = readFile(path.resolve(__dirname, '../__fixtures__', 'stylish_expected.txt'));
-const plainExpected = readFile(path.resolve(__dirname, '../__fixtures__', 'plain_expected.txt'));
-// const jsonExpected = readFile(path.resolve(__dirname, '../__fixtures__', 'json_expected.txt'));
+const paths = [
+  path.resolve(__dirname, '../__fixtures__', 'recursive1.json'),
+  path.resolve(__dirname, '../__fixtures__', 'recursive2.json'),
+  path.resolve(__dirname, '../__fixtures__', 'recursive1.yaml'),
+  path.resolve(__dirname, '../__fixtures__', 'recursive2.yaml'),
+  path.resolve(__dirname, '../__fixtures__', 'stylish_expected.txt'),
+  path.resolve(__dirname, '../__fixtures__', 'plain_expected.txt'),
+];
 
-test('JSON diff test', () => {
-  const filepath1 = path.resolve(__dirname, '../__fixtures__', 'recursive1.json');
-  const filepath2 = path.resolve(__dirname, '../__fixtures__', 'recursive2.json');
+const stylishResultJson = genDiff(paths[0], paths[1]);
+const stylishResultYaml = genDiff(paths[2], paths[3]);
+const plainResultJson = genDiff(paths[0], paths[1], 'plain');
+const plainResultYaml = genDiff(paths[2], paths[3], 'plain');
 
-  expect(genDiff(filepath1, filepath2)).toEqual(stylishExpected);
-  expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainExpected);
-  // expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonExpected);
-});
+const stylishExpected = readFile(paths[4]);
+const plainExpected = readFile(paths[5]);
 
-test('YAML diff test', () => {
-  const filepath1 = path.resolve(__dirname, '../__fixtures__', 'recursive1.yaml');
-  const filepath2 = path.resolve(__dirname, '../__fixtures__', 'recursive2.yaml');
+describe('gendiff test', () => {
+  test.each([
+    [stylishResultJson, stylishResultYaml, stylishExpected],
+    [plainResultJson, plainResultYaml, plainExpected],
+  ])('stylish && plain formats', (result1, result2, expected) => {
+    expect(result1).toEqual(expected);
+    expect(result2).toEqual(expected);
+  });
 
-  expect(genDiff(filepath1, filepath2)).toEqual(stylishExpected);
-  expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainExpected);
-  // expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonExpected);
+  test('json format', () => {
+    const data = genDiff(paths[0], paths[1], 'json');
+    expect(() => JSON.parse(data)).not.toThrow();
+  });
 });
